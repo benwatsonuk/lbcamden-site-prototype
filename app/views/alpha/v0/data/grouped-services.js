ae7874229aeb5afc05465091ef0f69a9f7128821
benwatsonuk <hello@benwatson.eu>
2022-06-28 09:19:54 +0100

    Grouped items in place

--- app/views/alpha/v0/data/grouped-services.js
+++ app/views/alpha/v0/data/grouped-services.js
@@ -3,7 +3,124 @@ module.exports = [
     title: 'Report',
     slug: 'report',
     description: 'Housing repairs, problems, complaints and more',
-    items: []
+    items: [
+      {
+        title: 'Housing',
+        items: [
+          {
+            title: 'Housing repairs',
+            href: '#',
+            description: 'Report a repair online for your council home.'
+          },
+          {
+            title: 'Unlawful occupancy',
+            href: '#',
+            description: 'Report an unlawful occupancy in a council home.'
+          }
+        ]
+      },
+      {
+        title: 'Health & safety',
+        items: [
+          {
+            title: 'Hate crime',
+            href: '#',
+            description: 'Find out about hate crime and how to report it.'
+          },
+          {
+            title: 'Antisocial behaviour',
+            href: '#',
+            description: 'If antisocial behaviour (ASB) is causing alarm or distress to you or the community where you live you should report this to the Camden Community Safety Team.'
+          },
+          {
+            title: 'Needles and drug paraphernalia',
+            href: '#',
+            description: 'Tell us about drug rubbish to be cleared from a Camden street.'
+          },
+          {
+            title: 'Dangerous structure',
+            href: '#',
+            description: 'Call 020 7974 4444 to report a dangerous structure. If you are a council tenant or leaseholder contact your housing officer.'
+          },
+          {
+            title: 'Report a hazard in your privately rented home',
+            href: '#',
+            description: 'Report a hazard under the Housing Health and Safety Rating System.'
+          },
+          {
+            title: 'Report a work or business related health and safety problem',
+            href: '#',
+            description: 'Report a work or business related health and safety problem'
+          }
+        ]
+      },
+      {
+        title: 'Environmental and street issues',
+        items: [
+          {
+            title: 'Issue with noise',
+            href: '#',
+            description: 'Report a noise complaint and find out more about noise pollution.'
+          },
+          {
+            title: 'Pest control',
+            href: '#',
+            description: 'Report a pest problem and find out about pest control pricing.'
+          },
+          {
+            title: 'Issue with smell',
+            href: '#',
+            description: 'Information on how to report a odour or bad smell'
+          },
+          {
+            title: 'Graffiti',
+            href: '#',
+            description: 'Tell us about illegal graffiti in Camden.'
+          },
+          {
+            title: 'Missed bin collection',
+            href: '#',
+            description: 'Let us know if we missed you.'
+          },
+          {
+            title: 'Fly-tipping and street obstructions',
+            href: '#',
+            description: 'Tell us about fly-tipping.'
+          },
+          {
+            title: 'Street damage or a cleaning issue',
+            href: '#',
+            description: 'Tell us about a problem on a Camden street.'
+          },
+          {
+            title: 'Abandoned vehicles',
+            href: '#',
+            description: 'Tell us about an abandoned vehicle.'
+          },
+          {
+            title: 'Leaf fall',
+            href: '#',
+            description: 'Tell us about a leaf-fall problem.'
+          }
+        ]
+      },
+      {
+        title: 'Other',
+        items: [
+          {
+            title: 'Report a breach of planning regulations',
+            href: '#',
+            description: 'How to report a suspected breach of planning regulations. How to find out whether the required planning permission has already been given for a development.'
+          },
+          {
+            title: 'Report an accessibility issue on the website',
+            href: '#',
+            description: 'Report an accessibility issue on the website.'
+          }
+        ]
+      }
+
+    ]
   },
   {
     title: 'Pay',
