e59267dc9fc4ac709d34f9657a8b61e7619356e2
benwatsonuk <hello@benwatson.eu>
2022-06-28 09:09:14 +0100

    Service page progress

--- app/views/alpha/v0/data/grouped-services.js
+++ app/views/alpha/v0/data/grouped-services.js
@@ -1,21 +1,25 @@
 module.exports = [
   {
     title: 'Report',
+    slug: 'report',
     description: 'Housing repairs, problems, complaints and more',
     items: []
   },
   {
     title: 'Pay',
+    slug: 'pay',
     description: 'Council tax, rent, business rates, PCN and more',
     items: []
   },
   {
     title: 'Apply',
+    slug: 'apply',
     description: 'Parking permits, council tax support, freedom pass, and more',
     items: []
   },
   {
     title: 'Request',
+    slug: 'request',
     description: 'Voting, birth certificates, bins, and more',
     items: []
   }
