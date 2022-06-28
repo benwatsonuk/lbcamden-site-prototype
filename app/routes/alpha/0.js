538eacbe7ebb243eca648243a508f14504a83d36
benwatsonuk <hello@benwatson.eu>
2022-06-28 09:15:50 +0100

    Basic routing in place

--- app/routes/alpha/0.js
+++ app/routes/alpha/0.js
@@ -15,8 +15,27 @@ if (sortServicesBy === 'orderValue') {
 module.exports = function (router) {
   router.use((req, res, next) => {
     res.locals.versionDirectory = versionDirectory
-    res.locals.services = serviceItems
+    res.locals.serviceItems = serviceItems
     res.locals.groupedItems = groupedItems
+    res.locals.orderedServiceItems = [...serviceItems].sort((a, b) => a.position - b.position)
     next()
+  })
+
+  // Category
+  router.get(['/alpha/' + versionDirectory + '/category/:serviceSlug'], (req, res) => {
+    const theServiceSlug = req.params.serviceSlug
+    const theService = serviceItems.find(x => (x.slug === theServiceSlug))
+    res.render('alpha/' + versionDirectory + '/category/index.html', {
+      service: theService
+    })
+  })
+
+  // Grouped
+  router.get(['/alpha/' + versionDirectory + '/grouped/:groupSlug'], (req, res) => {
+    const theGroupSlug = req.params.groupSlug
+    const theGroup = groupedItems.find(x => (x.slug === theGroupSlug))
+    res.render('alpha/' + versionDirectory + '/grouped/index.html', {
+      groupedItem: theGroup
+    })
   })
 }
