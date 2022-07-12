a8e1d3df4cef8f4c33af92ea41bf5fc1d6032a80
benwatsonuk <hello@benwatson.eu>
2022-07-12 18:29:21 +0100

    Basic header alternative

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,17 @@ if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
+$('#menu-toggle').click(() => {
+  $('.LBCamden-Header--nav').toggle()
+  $('.LBCamden-Header--search').hide()
+})
+
+$('#search-toggle').click(() => {
+  $('.LBCamden-Header--search').toggle()
+  $('.LBCamden-Header--nav').hide()
+  $('#search-main-b48c4de8').focus()
+})
+
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
   window.LBCamdenFrontend.initAll()
